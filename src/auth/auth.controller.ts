290cbbf6005ad53ca6ea46d249dee63c8277685b
OriShefer <ori150403@gmail.com>
2023-11-10 18:03:51 +0200

    add auth dto and axios

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller,ParseIntPipe, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
     constructor(private authService: AuthService){}
 
     @Post('signup')
-    signup(){
+    signup(@Body() dto: AuthDto){
         return this.authService.signup()
     }
 
@@ -14,4 +15,5 @@ export class AuthController{
     signin(){
         return this.authService.signin()
     }
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import axios from "axios";
 
 @Injectable()
 export class AuthService{
@@ -10,5 +11,37 @@ export class AuthService{
     signup(){
         return { msg: 'i am signed up'}
     }
+    
+
+    /*
+
+    async fetchDataFromHasura(){
+        const hasuraEndpoint = 'https://large-muskrat-86.hasura.app/v1/graphql';
+        const hasuraQuery = `mutation {
+            insert_twitter_Users(objects: [
+              {
+                user_id: 2,
+                username: "ori shefer",
+                password: "123",
+                join_date: "10.11.2023",
+                is_administrator: false
+              }
+            ]) {
+              affected_rows
+            }
+          }`
+
+          const headers = {
+            'Content-Type': 'application/json',
+            'x-hasura-admin-secret': "47yrZkJdGXnqOkShjD9A8PO9Y9PCL5PE2BPlDugPHKVu70cwmNcGv8gFymTshtAM",
+          };
+            
+          const response = await axios.post(hasuraEndpoint, { query: hasuraQuery }, { headers });        
+      
+        
+    return response;
+    }
+
+    */
 
 }
