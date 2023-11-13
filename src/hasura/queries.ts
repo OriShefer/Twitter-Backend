0f4f4b48a21e1a91dd8fdb31b27dbb2e9450652d
OriShefer <ori150403@gmail.com>
2023-11-13 23:04:30 +0200

    change queries

--- src/hasura/queries.ts
+++ src/hasura/queries.ts
@@ -3,8 +3,8 @@ import { gql } from 'graphql-tag';
 export const FETCH_DATA_QUERY = `mutation {
   insert_twitter_Users(objects: [
     {
-      user_id: 10,
-      username: "ori 123123",
+      user_id: 12,
+      username: "ori 123123213",
       password: "123",
       join_date: "10.11.2023",
       is_administrator: false
